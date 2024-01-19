d22ecdcb751829b065a4a92915a8445a6f0e46f6
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-01-19 13:13:21 -0300

    feat: add command `transactions view`

--- life/commands/transaction.py
+++ life/commands/transaction.py
@@ -17,8 +17,15 @@
 # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+from datetime import date
+from enum import Enum
 
+from rich import box
+from rich.table import Table
 from typer import Context, Typer
+
+from life.app import App
+from life.notion.filters import Date
 
 # ==============================================================================
 # GLOBAL
@@ -27,18 +34,66 @@ from typer import Context, Typer
 cli = Typer()
 
 # ==============================================================================
+# TYPES
+# ==============================================================================
+
+
+class Period(str, Enum):
+    week = "week"
+    month = "month"
+    year = "year"
+    future = "future"
+    all = "all"
+
+
+# ==============================================================================
 # TRANSACTIONS
 # ==============================================================================
 
 
 @cli.command("view")
-def transaction_view(
-    ctx: Context,
-):  # fmt: skip
+def transaction_view(ctx: Context, period: Period = Period.month):
     """
     View a summary of transactions on the terminal.
     """
-    raise NotImplementedError()
+    app: App = ctx.obj
+
+    filter = None
+    match period:
+        case Period.week:
+            filter = Date().this_week()
+        case Period.month:
+            filter = Date().this_month()
+        case Period.year:
+            filter = Date().this_week()
+        case Period.future:
+            filter = Date().after(date.today())
+
+    with app.working("Fetching transactions"):
+        transactions = app.db.transactions.query(filter).by_id()
+
+    if len(transactions) == 0:
+        app.error("No transactions in the selected period.").exit(0)
+
+    with app.working("Fetching accounts"):
+        accounts = app.db.accounts.query().by_id()
+
+    table = Table("Date", "Name", "Value", "Source", "Destination", box=box.HORIZONTALS)
+
+    for _, trn in transactions.items():
+        name = trn.name()
+        when = trn.date().start()
+        cash = trn.number("Value").value()
+        src = trn.relation("Source").value()
+        dst = trn.relation("Destination").value()
+        assert when is not None
+        when = when.strftime("%Y-%m-%d")
+        cash = f"R$ {cash:10.2f}"
+        src = accounts[src[0].id].name()
+        dst = accounts[dst[0].id].name()
+        table.add_row(when, name, cash, src, dst)
+
+    app.console.print(table)
 
 
 @cli.command("add")
